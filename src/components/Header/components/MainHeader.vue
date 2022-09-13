fe9eb453e0aefc81cbecd0955c4e6841a8c12197
ThalesTrombim <thalestr@outlook.com>
2022-09-13 20:27:55 -0300

    first commit

---
+++ src/components/Header/components/MainHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-header--container">
+    <div class="main-header--content">
+      <div class="main-header--logo">
+        <img src="../../../assets/logo-white.svg" alt="Nespresso">
+      </div>
+      <div class="main-header--userArea">
+        opções de login
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-header--container {
+  background: #000;
+}
+.main-header--content {
+  max-width: 996px;
+  margin: auto;
+  color: #FFF;
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+}
+.main-header--userArea {
+  color: #FFF;
+}
+.main-header--logo img{
+  width: 157px;
+}
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header-container">
+    <main-header></main-header>
+  </header>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue';
+
+export default {
+  name: 'the-header',
+  components: {
+    MainHeader
+  }
+}
+</script>
+
+<style scoped>
+.header-container {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+</style>
