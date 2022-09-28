b9afcb82c13dacf68da13fa64271951049d0d441
ThalesTrombim <thalestr@outlook.com>
2022-09-28 18:11:28 -0300

    feat: add shopcart items

--- src/components/ShopCart/index.vue
+++ src/components/ShopCart/index.vue
@@ -6,6 +6,14 @@
         X
       </div>
     </div>
+    <div class="shopcart-client-month">
+      <img src="../../assets/general/client-month.jpg" alt="Mês do cliente">
+      <span>Aproveite <strong>FRETE GRATIS</strong> no seu pedido.</span>
+    </div>
+    <div class="shopcart-items-area">
+      <span>Seu carrinho está vazio</span>
+      <button class="shopcart-shop-button">Iniciar compra</button>
+    </div>
   </div>
 </template>
 
@@ -23,8 +31,56 @@ export default {
     z-index: 1000;
     background: #FFF;
     height: 100vh;
+    width: 415px;
     right: 0;
+  }
+
+  &-header {
+    display: flex;
     padding: 20px;
+
+    p {
+      font-size: 18px;
+      font-weight: 700;
+      letter-spacing: .1em;
+      margin: 0;
+    }
+  }
+
+  &-client-month {
+    display: flex;
+    padding: 20px 0;
+    align-items: center;
+    flex-direction: column;
+    gap: 12px;
+    background: #F9F9F9;
+
+    img {
+      max-width: 300px;
+    }
+    span {
+      font-size: 12px
+    }
+  }
+
+  &-items-area {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    gap: 20px;
+  }
+
+  &-shop-button {
+    min-width: 335px;
+    padding: 12px 0;
+    border-radius: 3px;
+    border: none;
+    font-size: 13px;
+    font-family: 'Tahoma', sans-serif;
+    letter-spacing: .1em;
+    background: #3D8705;
+    color: #FFF;
   }
 }
 </style>
