7216b0765fefd06207839bf27d19cfe933730347
ThalesTrombim <thalestr@outlook.com>
2022-11-23 20:17:49 -0300

    feat: finish hero section

--- src/components/TheHero/index.vue
+++ src/components/TheHero/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="the-hero--container" 
-    :style="{ 'background-image': 'url(' + getImageUrl(background) + ')' }"
-  >
-    <div class="the-hero--content">
-      <span>
-        <strong>
-          AS OFERTAS MAIS ESPERADAS <br>
-          DO ANO JÁ COMEÇARAM
-        </strong>
-      </span>
-
-      <green-button 
-        link="#"
-        label="CONFIRA AS OFERTAS"
-      />
+  <div class="the-hero--section">
+    <div class="the-hero--header">
+      <h1>
+        CÁPSULAS E MÁQUINAS DE CAFÉ PARA SEU ESPRESSO PERFEITO
+      </h1>
+    </div>
+    <div class="the-hero--container" 
+      :style="{ 'background-image': 'url(' + getImageUrl(background) + ')' }"
+    >
+      <div class="the-hero--content">
+        <span>
+          <strong>
+            AS OFERTAS MAIS ESPERADAS <br>
+            DO ANO JÁ COMEÇARAM
+          </strong>
+        </span>
+  
+        <green-button 
+          link="#"
+          label="CONFIRA AS OFERTAS"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +40,15 @@ export default {
 
 <style lang="scss" scoped>
 .the-hero {
+  &--header {
+    display: flex;
+    justify-content: center;
+    h1 {
+      font-size: 19px;
+      letter-spacing: 3px;
+      margin: 20px;
+    }
+  }
   &--container {
     display: flex;
     align-items: center;
